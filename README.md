30623426e34640b00dbc41a10df784932768994b
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-07-27 15:39:17 +0900

    Frontend : index.html

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # SHARESPACE-Server
 SHARESPACE Server & Web app with Python Flask
+
+## Server (REST API with Flask-RESTful)
+
+### Newsfeed
+Provides a array of (title, image thumbnails)
+
+### Image infomation
+Provides infomation about one requested article
+
+- article title
+- article content
+- article tags
+- URL for image file
+
+## Web app
+
+## session
+Get input of id, email from user and store in session without further auth
--- app.py
+++ app.py
@@ -1,15 +1,33 @@
-from flask import Flask
-from flask_restful import Resource, Api
-
+from flask import Flask, render_template, redirect, request, flash, session, abort, url_for
+import os
 app = Flask(__name__)
-api = Api(app)
 
-class HelloWorld(Resource):
-    def get(self):
-        return {'hello': 'world'}
+@app.route('/')
+def home():
+    if not session.get('logged_in'):
+        return render_template('login.html')
+    else: # if logged in
+        return render_template('index.html', username=session.get('username'))
 
-api.add_resource(HelloWorld, '/')
+@app.route('/login', methods=['POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form.get('username')
+        session['email'] = request.form.get('email')
+        session['logged_in'] = True
+        return redirect(url_for('home'))
+    else:
+        return render_template('login.html')
+
+@app.route('/logout')
+def logout():
+    session['logged_in'] = False
+    return home()
+
+@app.errorhandler(404)
+def page_not_found(e): # error handler for 404
+    return render_template('404.html'), 404
 
 if __name__ == '__main__':
-    app.run(debug=True)
-    
+    app.secret_key = os.urandom(12) # generate random key
+    app.run(debug = True, host='0.0.0.0', port=5000)
