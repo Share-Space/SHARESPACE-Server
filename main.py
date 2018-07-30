1eabcbbaaa6259b34aaf3504a34bacb5a765e0ec
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-07-31 07:53:16 +0900

    Feature : With image_classification

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, redirect, request, url_for, Blueprint
 import os, base64
 from werkzeug import secure_filename
 from flask import current_app as app
+import image_classification
 
 main = Blueprint('main', __name__, template_folder='templates/main')
 ALLOWED_EXTENSIONS = set(['.jpg', '.jpeg'])
@@ -9,22 +10,22 @@ ALLOWED_EXTENSIONS = set(['.jpg', '.jpeg'])
 def allowed_file(filename):
     return os.path.splitext(filename)[1] in ALLOWED_EXTENSIONS
 
-@main.route('/view/<image_id>')
+@main.route('/view/<image_id>') # 현재는 그냥 파일명만 받게 됨
 def view(image_id):
-    image_id = base64.b64decode(image_id).decode()
+    image_path = base64.b64decode(image_id).decode()
     # for testing
     title = 'Petra'
     article = 'Petra, originally known to its inhabitants as Raqmu, is a historical and archaeological city in southern Jordan.'
-    tags = ['cool', 'wow', 'beautiful']
     # 요청받은 id의 파일에 대한 정보(제목, 본문, 태그)를 가져와 템플릿으로 전달해야 함
     # 태그 정보가 없을 경우, image_classification으로 태그를 생성해 저장함.
+    # print(image_path)
     return render_template(
         'vrview/view.html', 
-        image_url=image_id, 
-        current_url=request.base_url,
-        title=title,
-        article=article, 
-        tags=tags
+        image_url=image_path, # 이미지가 있는 정적 URL 주소
+        current_url=request.base_url, # 렌더링된 게시물의 URL (클립보드로 복사해 공유 기능을 제공)
+        title=title, article=article, # 게시물 제목 및 본문
+        tags=image_classification.classification(os.path.join(app.config['UPLOAD_FOLDER'], image_path))
+        # 이미지 인식 후 반환된 추론 결과(태그)를 템플릿에 전달
     )
 
 @main.route('/upload', methods=['GET', 'POST'])
