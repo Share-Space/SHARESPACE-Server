20eeb68e82e5a412bf5eb60ce916e821ebc5a2f6
JunhoYeo <32605822+JunhoYeo@users.noreply.github.com>
2018-07-30 16:14:12 +0900

    done : vrview template, fix : login btn

--- main.py
+++ main.py
@@ -3,7 +3,25 @@ import base64
 
 main = Blueprint('main', __name__, template_folder='templates/main')
 
-@main.route('/view', methods=['GET'])
-def view():
-    image_url = base64.b64decode(request.args.get('id')).decode()
-    return render_template('vrview/view.html', image_url=image_url)
+@main.route('/view/<image_id>')
+def view(image_id):
+    image_id = base64.b64decode(image_id).decode()
+    title = 'Petra'
+    article = 'Petra, originally known to its inhabitants as Raqmu, is a historical and archaeological city in southern Jordan.'
+    tags = ['cool', 'wow', 'beautiful']
+    return render_template(
+        'vrview/view.html', 
+        image_url=image_id, 
+        current_url=request.base_url,
+        title=title,
+        article=article, 
+        tags=tags
+    )
+
+@main.route('/upload')
+def upload():
+    return 'unavailable at the moment'
+
+@main.route('/search')
+def search():
+    return 'unavailable at the moment'
